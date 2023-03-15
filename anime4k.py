4b3238f017aa7ec82468d695ad1e214d05b984b2
Denis Mazur <kilerden2002@gmail.com>
2023-03-16 03:25:10 +1000

    Initial commit

---
+++ anime4k.py
@@ -0,0 +1,134 @@
+import os
+
+presets = {
+    'A (Fast)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_M.glsl', 'Anime4K_Upscale_CNN_x2_M.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_S.glsl'
+    ),
+    'B (Fast)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_Soft_M.glsl', 'Anime4K_Upscale_CNN_x2_M.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_S.glsl'
+    ),
+    'C (Fast)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Upscale_Denoise_CNN_x2_M.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+        'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_S.glsl'
+    ),
+    'A (HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_VL.glsl', 'Anime4K_Upscale_CNN_x2_VL.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_M.glsl'
+    ),
+    'B (HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_Soft_VL.glsl', 'Anime4K_Upscale_CNN_x2_VL.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_M.glsl'
+    ),
+    'C (HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Upscale_Denoise_CNN_x2_VL.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+        'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_M.glsl'
+    ),
+    'A+A (HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_VL.glsl', 'Anime4K_Upscale_CNN_x2_VL.glsl',
+        'Anime4K_Restore_CNN_M.glsl', 'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl',
+        'Anime4K_Upscale_CNN_x2_M.glsl'
+    ),
+    'B+B (HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_Soft_VL.glsl', 'Anime4K_Upscale_CNN_x2_VL.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Restore_CNN_Soft_M.glsl',
+        'Anime4K_Upscale_CNN_x2_M.glsl'
+    ),
+    'C+A (HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Upscale_Denoise_CNN_x2_VL.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+        'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Restore_CNN_M.glsl', 'Anime4K_Upscale_CNN_x2_M.glsl'
+    ),
+    'A (Ultra HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_GAN_UUL.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl'
+    ),
+    'B (Ultra HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_Soft_UL.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl'
+    ),
+    'C (Ultra HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Upscale_Denoise_CNN_x2_UL.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+        'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl'
+    ),
+    'A+A (Ultra HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_GAN_UUL.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl',
+        'Anime4K_Restore_GAN_UUL.glsl', 'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl',
+        'Anime4K_Upscale_CNN_x2_UL.glsl'
+    ),
+    'B+B (Ultra HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_CNN_Soft_UL.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl',
+        'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Restore_CNN_Soft_UL.glsl',
+        'Anime4K_Upscale_CNN_x2_UL.glsl'
+    ),
+    'C+A (Ultra HQ)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Upscale_Denoise_CNN_x2_UL.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+        'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Restore_GAN_UUL.glsl', 'Anime4K_Upscale_CNN_x2_UL.glsl'
+    ),
+    'A (Your PC will die)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Restore_GAN_UUL.glsl', 'Anime4K_Upscale_GAN_x4_UUL.glsl',
+        'Anime4K_Restore_CNN_Soft_M.glsl', 'Anime4K_Upscale_CNN_x2_M.glsl'
+    ),
+    'C+A (Your PC will die)': (
+        'Anime4K_Clamp_Highlights.glsl', 'Anime4K_Upscale_Denoise_CNN_x2_UL.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+        'Anime4K_AutoDownscalePre_x4.glsl', 'Anime4K_Restore_GAN_UUL.glsl', 'Anime4K_Upscale_GAN_x4_UUL.glsl',
+        'Anime4K_Restore_CNN_Soft_M.glsl'
+    )
+}
+
+qualities = ('S', 'M', 'L', 'VL', 'UL')
+modes = ('A', 'B', 'C', 'A+A', 'B+B', 'C+A')
+sec_qualities = {'S': 'S', 'M': 'S', 'L': 'M', 'VL': 'M', 'UL': 'L'}
+current_preset = ''
+
+
+def create_preset(quality, mode):
+    created_presets = {
+        'A': (
+            'Anime4K_Clamp_Highlights.glsl', f'Anime4K_Restore_CNN_{quality}.glsl',
+            f'Anime4K_Upscale_CNN_x2_{quality}.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+            'Anime4K_AutoDownscalePre_x4.glsl', f'Anime4K_Upscale_CNN_x2_{sec_qualities[quality]}.glsl'
+        ),
+        'B': (
+            'Anime4K_Clamp_Highlights.glsl', f'Anime4K_Restore_CNN_Soft_{quality}.glsl',
+            f'Anime4K_Upscale_CNN_x2_{quality}.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+            'Anime4K_AutoDownscalePre_x4.glsl', f'Anime4K_Upscale_CNN_x2_{sec_qualities[quality]}.glsl'
+        ),
+        'C': (
+            'Anime4K_Clamp_Highlights.glsl', f'Anime4K_Upscale_Denoise_CNN_x2_{quality}.glsl',
+            'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl',
+            f'Anime4K_Upscale_CNN_x2_{sec_qualities[quality]}.glsl'
+        ),
+        'A+A': (
+            'Anime4K_Clamp_Highlights.glsl', f'Anime4K_Restore_CNN_{quality}.glsl',
+            f'Anime4K_Upscale_CNN_x2_{quality}.glsl', f'Anime4K_Restore_CNN_{sec_qualities[quality]}.glsl',
+            'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl',
+            f'Anime4K_Upscale_CNN_x2_{sec_qualities[quality]}.glsl'
+        ),
+        'B+B': (
+            'Anime4K_Clamp_Highlights.glsl', f'Anime4K_Restore_CNN_Soft_{quality}.glsl',
+            f'Anime4K_Upscale_CNN_x2_{quality}.glsl', 'Anime4K_AutoDownscalePre_x2.glsl',
+            'Anime4K_AutoDownscalePre_x4.glsl', f'Anime4K_Restore_CNN_Soft_{sec_qualities[quality]}.glsl',
+            f'Anime4K_Upscale_CNN_x2_{sec_qualities[quality]}.glsl'
+        ),
+        'C+A': (
+            'Anime4K_Clamp_Highlights.glsl', f'Anime4K_Upscale_Denoise_CNN_x2_{quality}.glsl',
+            'Anime4K_AutoDownscalePre_x2.glsl', 'Anime4K_AutoDownscalePre_x4.glsl',
+            f'Anime4K_Restore_CNN_{sec_qualities[quality]}.glsl',
+            f'Anime4K_Upscale_CNN_x2_{sec_qualities[quality]}.glsl'
+        )
+    }
+    return created_presets[mode]
+
+
+def to_string(preset, preset_name = ''):
+    preset = (os.path.dirname(__file__) + os.sep + 'shaders' + os.sep + shader for shader in preset)
+    current_preset_name = preset_name
+    return ';'.join(preset)
+
+
+def android_config(preset, path='/storage/emulated/0/mpv/shaders/'):
+    if len(path) != 0 and path[-1] == '/':
+        path = path[0:-1]
+    preset = (path + '/' + shader for shader in preset)
+    return 'glsl-shaders=' + ':'.join(preset)
---
+++ localization.py
@@ -0,0 +1,49 @@
+import locale
+
+en = {
+    'File': 'File',
+    'Open file': 'Open file',
+    'Open URL': 'Open URL',
+    'Open folder': 'Open folder',
+    'Exit': 'Exit',
+    'Playback': 'Playback',
+    'Play | Pause': 'Play | Pause',
+    'Fullscreen': 'Fullscreen',
+    'Increasing image quality': 'Increasing image quality',
+    'Disable': 'Disable',
+    'Other': 'Other',
+    'Reference': 'Reference',
+    'About': 'About',
+    'Quality': 'Quality',
+    'Mode': 'Mode',
+    'About program': 'Media player written in Python programming language using PySimpleGUI library for graphical user interface, MPV media player and Anime4K scaling algorithm',
+    'Activate SVP': 'Activate SVP',
+    'Create config for Android': 'Create config for Android'
+}
+
+ru = {
+    'File': 'Файл',
+    'Open file': 'Открыть файл',
+    'Open URL': 'Открыть URL-адрес',
+    'Open folder': 'Открыть папку',
+    'Exit': 'Выход',
+    'Playback': 'Воспроизведение',
+    'Play | Pause': 'Воспроизведение | Пауза',
+    'Fullscreen': 'Полноэкранный режим',
+    'Increasing image quality': 'Увеличение качества изображения',
+    'Disable': 'Отключить',
+    'Other': 'Другое',
+    'Reference': 'Справка',
+    'About': 'О программе',
+    'Quality': 'Качество',
+    'Mode': 'Режим',
+    'About program': 'Медиаплеер, написанный на языке программирования Python с использованием библиотеки графического пользовательского интерфейса PySimpleGUI, мультимедийного проигрывателя MPV и алгоритма масштабирования Anime4K',
+    'Activate SVP': 'Активировать SVP',
+    'Create config for Android': 'Создать конфиг для Android'
+}
+
+match locale.getlocale()[0]:
+    case 'Russian_Russia':
+        strings = ru
+    case _:
+        strings = en
