6aa9fac1c9f56410e03e328cc16154cf7cedf297
Denis Mazur <kilerden2002@gmail.com>
2025-04-12 01:07:52 +1000

    Bug fix and changing icons

--- icons.py
+++ icons.py
@@ -1,10 +1,10 @@
 import os
 
-play = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}play_arrow_FILL0_wght400_GRAD0_opsz48.png'
-pause = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}pause_FILL0_wght400_GRAD0_opsz48.png'
-prev = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}skip_previous_FILL0_wght400_GRAD0_opsz48.png'
-next = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}skip_next_FILL0_wght400_GRAD0_opsz48.png'
-fullscreen = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}fullscreen_FILL0_wght400_GRAD0_opsz48.png'
-menu = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}list_FILL0_wght400_GRAD0_opsz48.png'
-audio = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}music_note_FILL0_wght400_GRAD0_opsz48.png'
-sub = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}subtitles_FILL0_wght400_GRAD0_opsz48.png'
+play = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}play_arrow_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+pause = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}pause_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+prev = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}skip_previous_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+next = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}skip_next_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+fullscreen = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}fullscreen_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+menu = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}format_list_bulleted_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+audio = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}music_note_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
+sub = f'{os.path.dirname(__file__) + os.sep}images{os.sep}icons{os.sep}subtitles_24dp_E3E3E3_FILL0_wght400_GRAD0_opsz24.svg'
